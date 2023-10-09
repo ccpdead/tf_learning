20916eea457dffac5809a108a73b11d88cd56f1d
jhr <weixinyii386@163.com>
2023-10-09 17:38:50 +0800

    add depth to program

--- 04-Python-learning/image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import pandas as pd
-
-image_path = "/home/jhr/depth_image1.png"
-csv_path="/home/jhr/depth.csv"
-
-img_input=cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
-data = pd.read_csv(csv_path)
-print(data)
